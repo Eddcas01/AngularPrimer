903a0d0eed1833b895a4f5d7b7f49ef4019cfb85
Eddcas01 <eddcas01@gmail.com>
2023-12-23 15:24:04 -0600

    Creacion de formularios con formcontrol, formgroup y formmodule, reactive forms

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,13 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, ],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
 
+  favoriteFramework: string = ''
+  formFormulario: FormGroup<any> = new FormGroup({
+
+    name:new FormControl(''),
+    email: new FormControl(''), 
+
+  });
+
+  handleFormSubmit(): void{
+alert(this.formFormulario.value);
+console.log(this.formFormulario.value);
+
+  }
+
+  enviarDatos(): void {
+
+    alert('este es tu framework favorito '  + this.favoriteFramework)
+
+  }
 }
---
+++ src/app/interfaces/formulario.ts
@@ -0,0 +1,4 @@
+export interface Formulario {
+    name: string;
+    email: string;
+}
